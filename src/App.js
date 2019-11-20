afc9d2a191e9727bd7baf65dcd063963c781d735
Facundo Arca <farca9@gmail.com>
2019-11-20 12:17:12 -0300

    Avances

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx"
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from "./components/header/header.component"
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import { ReactComponent as Logo } from '../../assets/crown.svg'  
+
+import './header.styles.scss';
+
+const Header = () => (
+
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            <Logo className='logo'></Logo>
+        </Link>
+        <h2>A VER SI ME CONTRATAN HDP PRIMER AVISO</h2> 
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+            </Link> 
+        </div>
+    </div>
+
+);
+
+export default Header;
