7ef5fc3255e2bfaec35ad16589952ba27f794a90
Facundo Arca <farca9@gmail.com>
2019-12-02 13:54:18 -0300

    Moved static shop info to reducers. Collection categories.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import SHOP_DATA from './shop.data'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-import PreviewCollection from '../../components/preview-collection/preview-collection.component.jsx'
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview></CollectionsOverview>
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        collections: SHOP_DATA
-    };
-  }
-  render(){
-      const {collections} = this.state;
-      return (
-        <div className='shop-page'>
-        {
-          collections.map(
-            ({id, ...otherCollectionProps}) => (
-              <PreviewCollection key={id} {...otherCollectionProps} />
-            )
-          )
-        }
-        </div>
-      )
-  }
-}
 
 export default ShopPage;
