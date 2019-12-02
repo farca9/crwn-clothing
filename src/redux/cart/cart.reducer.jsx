27b1d639e7503be4233e029041ae2ea26dc55ac7
Facundo Arca <farca9@gmail.com>
2019-12-02 12:34:46 -0300

    Checkout Item add and remove

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -8,7 +8,6 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
@@ -21,10 +20,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
 
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
+
+      case CartActionTypes.REMOVE_ITEM:
+        return{
+          ...state,
+          cartItems: removeItemFromCart(state.cartItems, action.payload)
+        }
     default:
       return state;
   }
-  
 };
 
 export default cartReducer;
