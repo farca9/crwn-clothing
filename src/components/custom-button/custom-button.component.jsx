674f4fe5a3ad43f29d14d4276440b94be06e3fcd
Facundo Arca <farca9@gmail.com>
2019-12-13 09:58:53 -0300

    Custom-button with inline styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
+import styles, { css } from 'styled-components';
 
-import './custom-button.styles.scss';
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
 
-const CustomButton = ({children, isGoogleSignIn, inverted,...otherProps}) => (
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
 
-    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-        {children}
-    </button>
+const googleSignInStyles = css`
+  background-color: #4285f4;
+  color: white;
+  border: none;
 
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+    color: white;
+  }
+`;
+
+const invertedButtonStyles = css`
+  &.inverted {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+
+    &:hover {
+      background-color: black;
+      color: white;
+      border: none;
+    }
+  }
+`;
+
+const getButtonStyles = props => {
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
+
+const CustomButtonContainer = styles.button`
+min-width: 165px;
+width: auto;
+height: 50px;
+letter-spacing: 0.5px;
+line-height: 50px;
+padding: 0 35px 0 35px;
+font-size: 15px;
+text-transform: uppercase;
+font-family: 'Yanone Kaffeesatz', sans-serif;
+font-weight: 600;
+cursor: pointer;
+display: flex;
+justify-content: center;
+
+${getButtonStyles}
+`;
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
